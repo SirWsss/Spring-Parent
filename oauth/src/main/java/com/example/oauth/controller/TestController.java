d87109c9522d78f289c583f5c9087f25c4d0c6b4
wang_qiang7 <litaibai66>
2020-05-23 09:14:43 +0800

    config配置中心读取文件从git源修改为本地源

--- oauth/src/main/java/com/example/oauth/controller/TestController.java
+++ oauth/src/main/java/com/example/oauth/controller/TestController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class TestController {
 
-    @Value("${name}")
+    @Value("${spring.datasource.username}")
     private String name;
 
     Logger log = LoggerFactory.getLogger(TestController.class);
